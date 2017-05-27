e5ad5687861ca5de4504c794c0f020f0a409dab1
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-05-27 07:04:42 +0300

    use an even simpler format
    Both reqToken and provToken are now just a 4-byte depToken which encodes
    only the name and the sence.  The package reference (used only with
    Requires) is encoded as a separate 4-byte number.  The same code
    can now be reused for both Requires and Provides.
    
    Also, there is now a single strtab, which stores Name+EVR as well as
    RequireVersion/ProvideVersion.  This enables the following optimization:
    although Name+EVR has to be copied into strtab in full, and should not
    be hashed into the lookup table, hashing just EVR saves another 0.5M.

--- unmets.c
+++ unmets.c
@@ -24,45 +24,83 @@
 #include <string.h>
 #include <assert.h>
 
-// The string tabs for %{RequireVersion} and %{ProvideVersion}.
-char depVerTab[256<<20];
-int depVerPos = 1;
+// The string tab for %{Name}+%{EVR}, %{RequireVersion} and %{ProvideVersion}.
+char strtab[256<<20];
+int strtabPos = 1;
 
-int addVer(const char *ver)
+int *lookupStrtab(const char *ver, size_t vlen)
 {
-    size_t vlen = strlen(ver);
-    assert(vlen);
     // Hash the version.
     unsigned b1 = (unsigned char) ver[vlen/2+0];
     unsigned b2 = (unsigned char) ver[vlen/2+1];
     unsigned hash = vlen;
     hash = 33 * hash + b1;
     hash = 33 * hash + b2;
-    // Lookup up in the cache.
+    // Get the hash chain which always has 2 indices into strtab[].
     static int lookup[1<<16][2];
-    int *va = lookup[hash % (1<<16)];
-    if (va[0] && memcmp(ver, depVerTab + vposa[0], vlen + 1) == 0)
+    return lookup[hash % (1<<16)];
+}
+
+int addVer(const char *ver)
+{
+    size_t vlen = strlen(ver);
+    assert(vlen);
+    // Lookup up in the cache.
+    int *va = lookupStrtab(ver, vlen);
+    if (va[0] && memcmp(ver, strtab + va[0], vlen + 1) == 0)
 	return va[0];
-    if (va[1] && memcmp(ver, depVerTab + vposa[1], vlen + 1) == 0)
+    if (va[1] && memcmp(ver, strtab + va[1], vlen + 1) == 0)
 	return va[1];
     // Store in the cache.
-    va[1] = vposa[0];
-    int vpos = va[0] = depVerPos;
-    assert(vpos + vlen + 1 < sizeof depVerTab);
-    memcpy(depVerTab + vpos, ver, vlen + 1);
-    depVerPos += vlen + 1;
+    va[1] = va[0];
+    int vpos = va[0] = strtabPos;
+    assert(vpos + vlen + 1 < sizeof strtab);
+    memcpy(strtab + vpos, ver, vlen + 1);
+    strtabPos += vlen + 1;
     return vpos;
+}
+
+#include <rpm/rpmlib.h>
+
+int addPkg(Header h)
+{
+    int pkgIx = strtabPos;
+
+    const char *name = headerGetString(h, RPMTAG_NAME);
+    assert(name);
+    size_t len = strlen(name);
+    assert(strtabPos + len + 1 < sizeof strtab);
+    memcpy(strtab + strtabPos, name, len);
+    strtabPos += len;
+    strtab[strtabPos++] = ' ';
+
+    char *ver = headerGetAsString(h, RPMTAG_EVR);
+    assert(ver);
+    size_t vlen = strlen(ver);
+
+    // Put EVR into the lookup cache.
+    int *va = lookupStrtab(ver, vlen);
+    if (!( (va[0] && memcmp(ver, strtab + va[0], vlen + 1) == 0) ||
+	   (va[1] && memcmp(ver, strtab + va[1], vlen + 1) == 0) ))
+	va[1] = va[0], va[0] = strtabPos;
+
+    assert(strtabPos + vlen + 1 < sizeof strtab);
+    memcpy(strtab + strtabPos, ver, vlen + 1);
+    strtabPos += vlen + 1;
+    free(ver);
+
+    return pkgIx;
 }
 
 // This program builds and processes two big sequences: the sequence
 // of Requires and the sequence of Provides (the latter also includes
 // %{Filenames}).  A sequence is made of variable-length records.
-// Each record starts with a token.
-struct provToken {
-    // 1 = lt | 2 = gt | 4 = eq
+// Each record starts with a 4-byte token.
+struct depToken {
+    // RPMSENSE_LESS | RPMSENSE_GREATER | RPMSENSE_EQUAL
     // 0 = no version
-    unsigned sense: 3;
-    // The sequence of Provides is sorted by name; the names are further
+    unsigned sense: 4;
+    // A sequence of dependencies is sorted by name; the names are further
     // front-encoded, roughly like in locatedb(5).  The delta field uses the
     // range of [-4095,4095] to encode the change in the common prefix length;
     // the value of -4096, if ever used, should reset the prefix length to 0.
@@ -73,79 +111,32 @@ struct provToken {
     // case, the 12-bit length limits the name to 4095 characters, which indeed
     // matches PATH_MAX.
     unsigned len: 12;
-    // Right after the token there goes a 4-byte version, unless sense is 0.
-    // And so Provide records look like this:
-    // - sense == 0:
-    //   +-+-+-+-+ +-    -+
-    //   | token | | name |
-    //   +-+-+-+-+ +-    -+
-    // - otherwise:
-    //   +-+-+-+-+ +-+-+-+-+ +-    -+
-    //   | token | |  ver  | | name |
-    //   +-+-+-+-+ +-+-+-+-+ +-    -+
 };
 
-// The string tab where package names are stored.
-char pkgNameTab[64<<20];
-int pkgNamePos;
+static_assert(sizeof(struct depToken) == 4, "depToken");
+static_assert((RPMSENSE_LESS | RPMSENSE_GREATER | RPMSENSE_EQUAL) < 16, "sense");
 
-// Package names are referenced multiple times (by each Require record
-// from a package, 9 of them on average).  Instead of identifying packages
-// by their offset in pkgNameTab[], the following table makes it possible
-// to identify packages by their number.  The maximum number of packages that
-// can be processed simultaneously is 1M, due to 20-bit indexes, see below.
-int pkgIxTab[1<<20];
-int pkgIxPos;
-
-// Requires dependencies are structured similarly to provides, except that
-// the token additionally stores the 20-bit index that identifies the package
-// the dependency came from.
-struct reqToken {
-    unsigned sense: 3;
-    int delta: 13;
-    unsigned len: 12;
-    unsigned pkg: 20;
-    // Right after the token there goes a 4-byte version, unless sense is 0.
-    // And so Require records look like this:
-    // - sense == 0:
-    //   +-+-+-+-+-+-+ +-    -+
-    //   |   token   | | name |
-    //   +-+-+-+-+-+-+ +-    -+
-    // - otherwise:
-    //   +-+-+-+-+-+-+ +-+-+-+-+ +-    -+
-    //   |   token   | |  ver  | | name |
-    //   +-+-+-+-+-+-+ +-+-+-+-+ +-    -+
-} __attribute__((packed));
-
-static_assert(sizeof(struct provToken) == 4, "provToken");
-static_assert(sizeof(struct reqToken)  == 6, "reqToken");
-
-#include <rpm/rpmlib.h>
-
-// Add the package to pkgNameTab.
-int addPkg(Header h)
-{
-    int pkgIx = pkgIxPos;
-    pkgIxTab[pkgIxPos++] = pkgNamePos;
-
-    const char *name = headerGetString(h, RPMTAG_NAME);
-    assert(name);
-    size_t len = strlen(name);
-    assert(pkgNamePos + len + 1 < sizeof pkgNameTab);
-    memcpy(pkgNameTab + pkgNamePos, name, len);
-    pkgNamePos += len;
-    pkgNameTab[pkgNamePos++] = ' ';
-
-    char *EVR = headerGetAsString(h, RPMTAG_EVR);
-    assert(EVR);
-    len = strlen(EVR);
-    assert(pkgNamePos + len + 1 < sizeof pkgNameTab);
-    memcpy(pkgNameTab + pkgNamePos, EVR, len + 1);
-    pkgNamePos += len + 1;
-    free(EVR);
-
-    return pkgIx;
-}
+// Right after the token there goes a 4-byte version (its index in the
+// string tab), unless sense is 0.  And so Provide records look like this:
+// - sense == 0:
+//   +-+-+-+-+ +-    -+
+//   | token | | name |
+//   +-+-+-+-+ +-    -+
+// - otherwise:
+//   +-+-+-+-+ +-+-+-+-+ +-    -+
+//   | token | |  ver  | | name |
+//   +-+-+-+-+ +-+-+-+-+ +-    -+
+//
+// Require records additionally store a 4-byte package reference (to be
+// printed if the dependency turns out to be unmet).  They look like this:
+// - sense == 0:
+//   +-+-+-+-+ +-+-+-+-+ +-    -+
+//   | token | |  pkg  | | name |
+//   +-+-+-+-+ +-+-+-+-+ +-    -+
+// - otherwise:
+//   +-+-+-+-+ +-+-+-+-+ +-+-+-+-+ +-    -+
+//   | token | |  ver  | |  pkg  | | name |
+//   +-+-+-+-+ +-+-+-+-+ +-+-+-+-+ +-    -+
 
 #include <stdbool.h>
 #include "qsort.h"
@@ -181,27 +172,32 @@ void sortDeps(int n, const char **names, const char **versions, int *flags)
 
 #include "lcp.h"
 
-char frame[8<<20];
-
-void addReq(Header h, int pkgIx)
+// Pack either Requires (pkgIx != 0) or Provides (pkgIx == 0) into [p,end).
+// Returns next p.
+char *addDeps(Header h, int pkgIx, char *p, char *end)
 {
+    const int tags[2][3] = {
+	{ RPMTAG_REQUIRENAME, RPMTAG_REQUIREVERSION, RPMTAG_REQUIREFLAGS },
+	{ RPMTAG_PROVIDENAME, RPMTAG_PROVIDEVERSION, RPMTAG_PROVIDEFLAGS },
+    };
     struct rpmtd_s td1, td2, td3;
-    int rc = headerGet(h, RPMTAG_REQUIRENAME, &td1, HEADERGET_MINMEM);
+    int rc = headerGet(h, tags[!pkgIx][0], &td1, HEADERGET_MINMEM);
     // Assume that Requires must be present - someplace, they check
     // for the "rpmlib(PayloadIsLzma)" dependency as mandatory.
+    // Provides must also be present, due to "Provides: %name = %EVR".
     assert(rc == 1);
     assert(td1.count > 0);
     int n = td1.count;
     assert(td1.type == RPM_STRING_ARRAY_TYPE);
     const char **names = td1.data;
 
-    rc = headerGet(h, RPMTAG_REQUIREVERSION, &td2, HEADERGET_MINMEM);
+    rc = headerGet(h, tags[!pkgIx][1], &td2, HEADERGET_MINMEM);
     assert(rc == 1);
     assert(td2.count == n);
     assert(td2.type == RPM_STRING_ARRAY_TYPE);
     const char **versions = td2.data;
 
-    rc = headerGet(h, RPMTAG_REQUIREFLAGS, &td3, HEADERGET_MINMEM);
+    rc = headerGet(h, tags[!pkgIx][2], &td3, HEADERGET_MINMEM);
     assert(rc == 1);
     assert(td3.count == n);
     assert(td3.type == RPM_INT32_TYPE);
@@ -209,37 +205,37 @@ void addReq(Header h, int pkgIx)
 
     sortDeps(n, names, versions, flags);
 
-    char *p = frame, *end = p + sizeof frame;
     size_t lastNameLen = 0, lastLcpLen = 0;
     for (int i = 0; i < n; i++) {
 	// Make a token.
-	unsigned sense = (flags[i] & RPMSENSE_SENSEMASK) >> 1;
-	assert(sense < 8);
+	unsigned sense = flags[i] & (RPMSENSE_LESS | RPMSENSE_GREATER | RPMSENSE_EQUAL);
 	size_t nameLen = strlen(names[i]);
 	assert(nameLen < 4096);
 	size_t lcpLen = i ? lcp(names[i-1], lastNameLen, names[i], nameLen) : 0;
 	int delta = (int) lcpLen - (int) lastLcpLen;
 	size_t len1 = nameLen - lcpLen;
-	struct reqToken token = {
+	struct depToken token = {
 	    .sense = sense,
 	    .delta = delta,
 	    .len = len1,
-	    .pkg = pkgIx,
 	};
-	// Name not changed?
-	if (delta == 0 && len1 == 0)
+	// RequireName not changed?
+	if (pkgIx && delta == 0 && len1 == 0)
 	    // No version? It must be a dup then, as per depCmp ordering.
 	    // E.g. "Requires: /bin/sh" and "Requires(pre): /bin/sh".
 	    if (sense == 0)
 		continue;
 	// Put the record.
-	assert(p + 6 + (sense ? 4 : 0) + len1 < end);
-	memcpy(p, &token, 6);
-	p += 6;
+	assert(p + 4 + (sense ? 4 : 0) + (pkgIx ? 4 : 0) + len1 < end);
+	memcpy(p, &token, 4);
+	p += 4;
 	if (sense) {
-	    // Put version.
 	    int vpos = addVer(versions[i]);
 	    memcpy(p, &vpos, 4);
+	    p += 4;
+	}
+	if (pkgIx) {
+	    memcpy(p, &pkgIx, 4);
 	    p += 4;
 	}
 	memcpy(p, names[i] + lcpLen, len1);
@@ -250,165 +246,106 @@ void addReq(Header h, int pkgIx)
     rpmtdFreeData(&td1);
     rpmtdFreeData(&td2);
     rpmtdFreeData(&td3);
+
+    return p;
 }
 
-void addProv(Header h)
+char *addReq(Header h, int pkgIx, char *p, char *end)
+{
+    assert(pkgIx);
+    return addDeps(h, pkgIx, p, end);
+}
+
+char *addProv(Header h, char *p, char *end)
+{
+    return addDeps(h, 0, p, end);
+}
+
+// Pack filenames (on behalf of Provides).
+char *addFnames(Header h, char *p, char *end)
 {
     struct rpmtd_s td1, td2, td3;
-    int rc = headerGet(h, RPMTAG_PROVIDENAME, &td1, HEADERGET_MINMEM);
-    // Assume that Provides must be present, due to "Provides: %name = %EVR".
-    assert(rc == 1);
-    assert(td1.count > 0);
-    int n = td1.count;
-    assert(td1.type == RPM_STRING_ARRAY_TYPE);
-    const char **names = td1.data;
-
-    rc = headerGet(h, RPMTAG_PROVIDEVERSION, &td2, HEADERGET_MINMEM);
-    assert(rc == 1);
-    assert(td2.count == n);
-    assert(td2.type == RPM_STRING_ARRAY_TYPE);
-    const char **versions = td2.data;
-
-    rc = headerGet(h, RPMTAG_PROVIDEFLAGS, &td3, HEADERGET_MINMEM);
-    assert(rc == 1);
-    assert(td3.count == n);
-    assert(td3.type == RPM_INT32_TYPE);
-    int *flags = td3.data;
-
-    sortDeps(n, names, versions, flags);
-
-    // Load filenames.
-    struct rpmtd_s td4, td5, td6;
     int bnc = 0;
     const char **bn = NULL, **dn = NULL;
     int *di = NULL;
-    if (headerGet(h, RPMTAG_BASENAMES, &td4, HEADERGET_MINMEM) == 1) {
-	assert(td4.type == RPM_STRING_ARRAY_TYPE);
-	assert(td4.count > 0);
-	bnc = td4.count;
-	bn = td4.data;
+    int rc = headerGet(h, RPMTAG_BASENAMES, &td1, HEADERGET_MINMEM);
+    if (rc != 1)
+	return p;
+    assert(td1.type == RPM_STRING_ARRAY_TYPE);
+    assert(td1.count > 0);
+    bnc = td1.count;
+    bn = td1.data;
 
-	rc = headerGet(h, RPMTAG_DIRNAMES, &td5, HEADERGET_MINMEM);
-	assert(rc == 1);
-	assert(td5.type == RPM_STRING_ARRAY_TYPE);
-	assert(td5.count > 0);
-	dn = td5.data;
+    rc = headerGet(h, RPMTAG_DIRNAMES, &td2, HEADERGET_MINMEM);
+    assert(rc == 1);
+    assert(td2.type == RPM_STRING_ARRAY_TYPE);
+    assert(td2.count > 0);
+    dn = td2.data;
 
-	rc = headerGet(h, RPMTAG_DIRINDEXES, &td6, HEADERGET_MINMEM);
-	assert(rc == 1);
-	assert(td6.type == RPM_INT32_TYPE);
-	assert(td6.count == bnc);
-	di = td6.data;
-    }
+    rc = headerGet(h, RPMTAG_DIRINDEXES, &td3, HEADERGET_MINMEM);
+    assert(rc == 1);
+    assert(td3.type == RPM_INT32_TYPE);
+    assert(td3.count == bnc);
+    di = td3.data;
 
-    // Filename stuff.
-    char fname[4096];
-    size_t fnameLen = 0;
-    bool validFname = false;
+    char fname[4096], lastFname[4096];
+    size_t fnameLen = 0, lastFnameLen = 0, lastLcpLen = 0;
     int lastDi = -1;
     size_t dirLen = 0;
 
-    // Either last Provides or fname.
-    const char *lastName = NULL;
-    char lastNameBuf[4096];
-    size_t lastNameLen = 0, lastLcpLen = 0;
-
-    // Merge Provides with filenames.
-    char *p = frame, *end = p + sizeof frame;
-    int i = 0, j = 0;
-    while (i < n || j < bnc) {
-	// Generate or update filename.
-	if (j < bnc && !validFname) {
-	    validFname = true;
-	    if (lastDi != di[j]) {
-		lastDi = di[j];
-		dirLen = strlen(dn[lastDi]);
-		assert(dirLen < 4096);
-		memcpy(fname, dn[lastDi], dirLen);
-	    }
-	    size_t bnLen = strlen(bn[j]);
-	    assert(dirLen + bnLen < 4096);
-	    memcpy(fname + dirLen, bn[j], bnLen + 1);
-	    fnameLen = dirLen + bnLen;
+    for (int i = 0; i < bnc; i++) {
+	if (lastDi != di[i]) {
+	    lastDi = di[i];
+	    dirLen = strlen(dn[lastDi]);
+	    assert(dirLen < 4096);
+	    memcpy(fname, dn[lastDi], dirLen);
 	}
-	// Find out name/version/sense to be encoded.
-	const char *name = NULL, *version = NULL;
-	size_t nameLen = 0;
-	unsigned sense = 0;
-	if (i < n && j < bnc) {
-	    int cmp = strcmp(names[i], fname);
-	    if (cmp <= 0) {
-		// Take Provides.
-		name = names[i], nameLen = strlen(name);
-		version = versions[i], sense = (flags[i] & RPMSENSE_SENSEMASK) >> 1;
-		i++;
-		// Advance filenames as well?
-		if (cmp == 0)
-		    j++, validFname = false;
-	    }
-	    else {
-		// Take Filename, leave Provides for the next iteration.
-		name = fname, nameLen = fnameLen, validFname = false;
-		j++;
-	    }
-	}
-	else if (i < n) {
-	    name = names[i], nameLen = strlen(name);
-	    version = versions[i], sense = (flags[i] & RPMSENSE_SENSEMASK) >> 1;
-	    i++;
-	}
-	else {
-	    assert(j < bnc);
-	    name = fname, nameLen = fnameLen, validFname = false;
-	    j++;
-	}
+	size_t bnLen = strlen(bn[i]);
+	fnameLen = dirLen + bnLen;
+	assert(fnameLen > 0);
+	assert(fnameLen < 4096);
+	memcpy(fname + dirLen, bn[i], bnLen + 1);
 	// Make a token.
-	assert(nameLen > 0);
-	assert(nameLen < 4096);
-	size_t lcpLen = i || j ? lcp(lastName, lastNameLen, name, nameLen) : 0;
+	size_t lcpLen = i ? lcp(lastFname, lastFnameLen, fname, fnameLen) : 0;
 	int delta = (int) lcpLen - (int) lastLcpLen;
-	size_t len1 = nameLen - lcpLen;
-	struct provToken token = {
-	    .sense = sense,
+	size_t len1 = fnameLen - lcpLen;
+	struct depToken token = {
+	    .sense = 0,
 	    .delta = delta,
 	    .len = len1,
 	};
 	// Put the record.
-	assert(p + 4 + (sense ? 4 : 0) + len1 < end);
+	assert(p + 4 + len1 < end);
 	memcpy(p, &token, 4);
 	p += 4;
-	if (sense) {
-	    // Put version.
-	    int vpos = addVer(version);
-	    memcpy(p, &vpos, 4);
-	    p += 4;
-	}
-	memcpy(p, name + lcpLen, len1);
+	memcpy(p, fname + lcpLen, len1);
 	p += len1;
-	// Copy fname for lastName.
-	lastName = name == fname ? memcpy(lastNameBuf, name, nameLen + 1) : name;
-	lastNameLen = nameLen, lastLcpLen = lcpLen;
+	// Copy fname to lastFname.
+	memcpy(lastFname, fname, fnameLen + 1);
+	lastFnameLen = fnameLen, lastLcpLen = lcpLen;
     }
 
     rpmtdFreeData(&td1);
     rpmtdFreeData(&td2);
     rpmtdFreeData(&td3);
-    if (bnc) {
-	rpmtdFreeData(&td4);
-	rpmtdFreeData(&td5);
-	rpmtdFreeData(&td6);
-    }
+
+    return p;
 }
 
 int verbose;
+int npkg;
+
+char frame[8<<20];
 
 void addHeader(Header h)
 {
     int pkgIx = addPkg(h);
-    addReq(h, pkgIx);
-    addProv(h);
+    addReq(h, pkgIx, frame, frame + sizeof frame);
+    addProv(h, frame, frame + sizeof frame);
+    addFnames(h, frame, frame + sizeof frame);
     if (verbose > 1)
-	fprintf(stderr, "loaded %s\n", pkgNameTab + pkgIxTab[pkgIx]);
+	fprintf(stderr, "loaded %s\n", strtab + pkgIx);
+    npkg++;
 }
 
 #include <stdbool.h>
@@ -451,9 +388,8 @@ int main(int argc, char **argv)
     }
     Fclose(Fd);
     if (verbose)
-	fprintf(stderr, "loaded %d headers (%.1fM out of %zuM pkgTab, %.1fM out of %zuM verTab)\n", pkgIxPos,
-			(double) pkgNamePos / (1 << 20), sizeof pkgNameTab >> 20,
-			(double) depVerPos / (1 << 20), sizeof depVerTab >> 20);
+	fprintf(stderr, "loaded %d headers (%.1fM strtab)\n", npkg,
+			(double) strtabPos / (1 << 20));
     return 0;
 }
 
